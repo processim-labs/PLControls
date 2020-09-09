3110ed1fc8eaefce74aa0af384ec113657f4e58a
Hans Gatgens <hans.gatgens@processimlabs.com>
2020-09-09 10:05:07 -0600

    Bindable content for view

--- src/Test/Test/Controls/ContentButton.xaml.cs
+++ src/Test/Test/Controls/ContentButton.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using Xamarin.Forms.Xaml;
 namespace Test.Controls
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    [ContentProperty("ButtonContent")]
     public partial class ContentButton : ContentView
     {
         public ContentButton()
@@ -23,7 +25,14 @@ namespace Test.Controls
             set => SetValue(CommandProperty, value);
         }
 
+        public View ButtonContent 
+        {
+            get => (View)GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+
         public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(ContentButton), null, propertyChanged: (bindable, oldValue, newValue) => ((ContentButton)bindable).OnClickOrTouchedDownCommandPropertyChanged());
+        public static readonly BindableProperty ButtonContentProperty = BindableProperty.Create(nameof(ButtonContent), typeof(View), typeof(ContentButton), propertyChanged: ButtonContentChanged);
 
         public object CommandParameter
         {
@@ -33,6 +42,22 @@ namespace Test.Controls
 
         public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(ContentButton), null, propertyChanged: (bindable, oldValue, newValie) => ((ContentButton)bindable).CommandCanExecuteChanged(bindable, EventArgs.Empty));
 
+        private static void ButtonContentChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            if (newvalue != null) 
+            {
+                var control = (ContentButton)bindable;
+                var value = (View)newvalue;
+
+                if (control.container == null) 
+                    Debug.WriteLine("ViewContent null!");
+
+                if (ReferenceEquals(newvalue, control))
+                    Debug.WriteLine("New value is myself!!!!");
+
+                control.container.Content = value;
+            }
+        }
 
         protected void CommandCanExecuteChanged(object sender, EventArgs e)
         {
